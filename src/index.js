455214334b31195ec821808b194a8218d9f321a9
lz-junior <correajr.dev@gmail.com>
2024-05-07 18:33:29 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.get('/', (req, res) => {
+  res.send("app GET")
+})
+
+app.post('/', (req, res) => {
+  res.send("app POST")
+})
+
+app.put('/', (req, res) => {
+  res.send("app PUT")
+})
+
+app.delete('/', (req, res) => {
+  res.send("app DELETE")
+})
+
+
 app.listen(PORT, () => {
   console.log(`App online na porta ${PORT}`)
 })
